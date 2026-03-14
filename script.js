7385e9b001664862ada48ef9ac118068a31fbf61
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-03-14 14:15:00 -0300

    add - Correções de erros e adicionei a ultima atividade que é um complemento da 10. Atividade onze trabalha com diferentes desconto conforme o salário.

--- script.js
+++ script.js
@@ -247,7 +247,7 @@ function atividade_10(){
 
     let nome_funcionario = (prompt)("Digite o nome do funcionário ");
     let salario_bruto = parseFloat(prompt)("Digite o salário bruto ");
-    let taxa_desconto = 0.8;
+    let taxa_desconto = 0.08;
     let desconto = salario_bruto * taxa_desconto;
     let salario_liquido = salario_bruto - desconto;
 
@@ -259,6 +259,36 @@ alert("Valor do desconto: " + desconto);
 alert("O salario liquido é: " + salario_liquido);
 }
 
+
+function atividade_11(){
+ 
+    let salario_bruto = parseFloat(prompt)("Digite o salário bruto ");
+    let taxa_desconto = 0.08;
+    let taxa_desconto2 = 0.085;
+    let taxa_desconto3 = 0.09;
+
+   if( salario_bruto = 1000){
+    let desconto = salario_bruto * taxa_desconto;
+        let salario_liquido = salario_bruto - desconto;
+            alert("Valor do desconto: " + desconto);
+            alert("O salario liquido é: " + salario_liquido);
+
+   }else if (salario_bruto  <= 1500){
+    let desconto2 = salario_bruto * taxa_desconto2;
+        let salario_liquido = salario_bruto - desconto2;
+
+        alert("Valor do desconto: " + desconto2);
+            alert("O salario liquido é: " + salario_liquido);
+   }
+else{
+    let desconto3 = salario_bruto * taxa_desconto3;
+        let salario_liquido = salario_bruto - desconto3;
+
+        alert("Valor do desconto: " + desconto3);
+            alert("O salario liquido é: " + salario_liquido);
+
+}
+}
 
 
 
@@ -274,4 +304,5 @@ atividade_7();
 atividade_8();
 atividade_9();
 atividade_10();
+atividade_11();
 
