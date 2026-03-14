2e38f39b41ef43d7829c1e9a48b16e13803ee523
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-03-14 13:25:18 -0300

    add - Adicionei a atividade 6 e 7. Atividade 6 sobre as letras do alfabeto e a 7 sobre sistemas de preços.

--- script.js
+++ script.js
@@ -145,6 +145,64 @@ alert("Agora o número impar é um numero par = " + transformacao_2);
  }
 }
 
+function atividade_6(){
+
+ let letra = (prompt("Digite uma letra do alfabeto "));
+
+if( letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u"){
+    alert("Está letra é uma vogal");
+    console.log("Está letra é uma vogal");
+
+}else{
+    alert("Está letra é uma consoante");
+    console.log("Está letra é uma consoante");
+}
+}
+
+
+function atividade_7(){
+
+    let codigo = (prompt)("Digite o código do produto de (a até i)");
+
+    switch(codigo){
+case "a":
+        console.log("Sabor - Chocolate - R$1,50")
+break;
+
+case "b":
+        console.log("Sabor - Morango - R$2,50")
+break;
+
+case "c":
+        console.log("Sabor - Creme - R$2,50")
+break;
+
+case "d":
+        console.log("Sabor - Manga - R$3,20")
+break;
+
+case "e":
+        console.log("Sabor - Melancia - R$3,40")
+break;
+
+case "f":
+        console.log("Sabor - Vanilla Ice - R$3,00")
+break;
+
+case "g":
+        console.log("Sabor - Céu Azul - R$3,60")
+break;
+
+case "h":
+        console.log("Sabor - Brownie - R$4,00")
+break;
+
+case "i":
+        console.log("Sabor - Hawaiano - R$5,00")
+break;
+
+    }
+}
 
 
 
@@ -161,7 +219,7 @@ contagem_nasa();
 atividade_3();
 atividade_4();
 atividade_5();
-
+atividade_6();
 
 
 
